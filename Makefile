b397ebcdc6537703c9b1b5d0a36ccc8580ecacc2
Marek Belisko <marek.belisko@gmail.com>
2009-04-28 16:05:35 +0200

    First version of iqrf loader implemented. Sources moved to root directory. Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

--- Makefile
+++ Makefile
@@ -4,15 +4,20 @@ LDFLAGS = -lusb -lrt
 
 
 EXEC = spi_example
-OBJS = usb.o
+OBJS = spi_example.o
 
-all: $(EXEC)
+EXEC1 = liqrf
+OBJS1 = usb.o hex_parser.o spi.o liqrf.o iqrf.o
+
+all: $(EXEC) $(EXEC1)
 
 $(EXEC): $(OBJS)
 	$(CC) $(LDFLAGS) -o $@ $(OBJS)
 
-
+$(EXEC1): $(OBJS1)
+	$(CC) $(LDFLAGS) -o $@ $(OBJS1)
 clean:
 	-rm -f $(EXEC) *.elf *.gdb *.o
+	-rm -f $(EXEC1) *.elf *.gdb *.o
 	-rm -f *.h~ *.c~
 
--- usb.c
+++ usb.c
@@ -18,87 +18,45 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #include <stdio.h>
-#include <errno.h>
-#include <usb.h>
-#include <string.h>
-#include <time.h>
-#include <signal.h>
-#include <ctype.h>
 
-#include "iqrf.h"
+#include "usb.h"
 
-#define DEBUG
+#define debug_usb(format, arg...) \
+	if (verbose) {\
+		printf("usb:" format , ## arg); \
+	} else {}\
+	 	 
+#define debug_usb_notext(format, arg...) \
+	if (verbose) {\
+		printf(format , ## arg); \
+	} else {}\
 
-#ifdef DEBUG
-int dbg_level = 3;
-#else
-int dbg_level = 0;
-#endif
-
-/* object for holding global variables */
-struct liqrf_obj {
-	struct usb_device *dev;
-	struct usb_dev_handle *dev_handle;
-	unsigned char rx_buff[BUF_LEN];
-	unsigned char tx_buff[BUF_LEN];
-	int tx_len;
-	int rx_len;
-	int master_only_read;
-};
-
-unsigned char master_only_read, spi_check = 0;
-
-struct liqrf_obj iqrf_dev;
-struct liqrf_obj *iqrf = &iqrf_dev;
-
-int err = 1;
-
-#ifdef DEBUG
-void printDevDesc(struct usb_device *dev)
+/* print some debugging info */
+void print_dev_desc(struct usb_device *dev)
 {
-	printf("#	bDescriptorType: %d (0x%x)\n", dev->descriptor.bDescriptorType,
+	debug_usb("#	bDescriptorType: %d (0x%x)\n", dev->descriptor.bDescriptorType,
 	       dev->descriptor.bDescriptorType);
-	printf("#	bcdUSB: %d (0x%x)\n", dev->descriptor.bcdUSB,
+	debug_usb("#	bcdUSB: %d (0x%x)\n", dev->descriptor.bcdUSB,
 	       dev->descriptor.bcdUSB);
-	printf("#	bDeviceProtocol: %d (0x%x)\n", dev->descriptor.bDeviceProtocol,
+	debug_usb("#	bDeviceProtocol: %d (0x%x)\n", dev->descriptor.bDeviceProtocol,
 	       dev->descriptor.bDeviceProtocol);
-	printf("#	idVendor: %d (0x%x)\n", dev->descriptor.idVendor,
+	debug_usb("#	idVendor: %d (0x%x)\n", dev->descriptor.idVendor,
 	       dev->descriptor.idVendor);
-	printf("#	idProduct: %d (0x%x)\n", dev->descriptor.idProduct,
+	debug_usb("#	idProduct: %d (0x%x)\n", dev->descriptor.idProduct,
 	       dev->descriptor.idProduct);
-	printf("#	bcdDevice: %d (0x%x)\n", dev->descriptor.bcdDevice,
+	debug_usb("#	bcdDevice: %d (0x%x)\n", dev->descriptor.bcdDevice,
 	       dev->descriptor.bcdDevice);
-	printf("#	iManufacturer: %d (0x%x)\n", dev->descriptor.iManufacturer,
+	debug_usb("#	iManufacturer: %d (0x%x)\n", dev->descriptor.iManufacturer,
 	       dev->descriptor.iManufacturer);
-	printf("#	iProduct: %d (0x%x)\n", dev->descriptor.iProduct,
+	debug_usb("#	iProduct: %d (0x%x)\n", dev->descriptor.iProduct,
 	       dev->descriptor.iProduct);
-	printf("#	bNumConfigurations: %d (0x%x)\n",dev->descriptor.bNumConfigurations,
+	debug_usb("#	bNumConfigurations: %d (0x%x)\n",dev->descriptor.bNumConfigurations,
 		dev->descriptor.bNumConfigurations);
 }
 
-
-void printStrings(void)
-{
-	char sManufacturer[256];
-	char sProduct[256];
-
-	if(usb_get_string_simple(iqrf->dev_handle, iqrf->dev->descriptor.iManufacturer, 
-		sManufacturer, sizeof(sManufacturer)))
-		printf("#	Manufacturer string: %s\n", sManufacturer);
-	else
-		printf("#	Cannot get manufacturer string.\n");
-
-	if(usb_get_string_simple(iqrf->dev_handle, iqrf->dev->descriptor.iProduct, sProduct, sizeof(sProduct)))
-		printf("#	Product string: %s\n", sProduct);
-	else
-		printf("#	Cannot get product string.\n");
-}
-#endif
-
 /* device initialization */
-static struct usb_device *liqrf_device_init(void)
+struct usb_device *iqrf_device_init(void)
 {
 	struct usb_bus *usb_bus;
 	struct usb_device *dev;
@@ -113,10 +71,10 @@ static struct usb_device *liqrf_device_init(void)
 		for (dev = usb_bus->devices; dev; dev = dev->next) {
 			if ((dev->descriptor.idVendor == IQRF_VENDOR_ID) &&
 			    (dev->descriptor.idProduct == IQRF_PRODUCT_ID)) {
-#ifdef DEBUG
+
 				if (dev != NULL)
-					printDevDesc(dev);
-#endif	
+					print_dev_desc(dev);
+	
 				return dev;
 			}
 		}
@@ -126,220 +84,64 @@ static struct usb_device *liqrf_device_init(void)
 }
 
 /* write to output endpoint and read
-   data from input endpoint */
-static int liqrf_send_receive_packet(struct liqrf_obj *iqrf)
+   data from input endpoint 
+ */
+int send_receive_packet(struct usb_dev_handle *dev_handle, 
+				char *tx_buff, int tx_len, char *rx_buff, int rx_len)
 {
 	int ret_val = 0, i = 0;
 
-	ret_val = usb_interrupt_write(iqrf->dev_handle, OUT_EP_NR,
-				      (char *)iqrf->tx_buff, iqrf->tx_len,
+	debug_usb("Write data len = %d:\n", tx_len);
+	for (i = 0; i < tx_len; i++)
+		debug_usb_notext("%02X ", (unsigned char)tx_buff[i]);
+	debug_usb_notext("\n");
+
+	ret_val = usb_interrupt_write(dev_handle, OUT_EP_NR,
+				      tx_buff, tx_len,
 				      32000);
 
 	if (ret_val < 0) {
 		fprintf(stderr, "Can't write to output endpoint\n");
 		goto exit;
 	}
-#ifdef DEBUG
-	printf("Written data:\n");
-	for (i = 0; i < iqrf->tx_len; i++)
-		printf("%2X ", iqrf->tx_buff[i]);
-	printf("\n");
-#endif
-	ret_val = usb_interrupt_read(iqrf->dev_handle, IN_EP_NR,
-				     (char *)iqrf->rx_buff, iqrf->rx_len,
-				     32000);
 
+	ret_val = usb_interrupt_read(dev_handle, IN_EP_NR,
+				     rx_buff, rx_len,
+				     32000);
+	
 	if (ret_val < 0) {
 		fprintf(stderr, "Can't read from input endpoint\n");
 		goto exit;
 	}
-#ifdef DEBUG
-	printf("Readed string:\n");
-	for (i = 0; i < ret_val; i++) {
-		if (isprint(iqrf->rx_buff[i]))
-			printf("%c ", iqrf->rx_buff[i]);
-		else
-			printf("%X ", iqrf->rx_buff[i]);
-	}
-	printf("\n");
-#endif
+
+	debug_usb("Read string len = %d:\n", ret_val);
+	for (i = 0; i < ret_val; i++) 
+		debug_usb_notext("%02X ", (unsigned char)rx_buff[i]);
+	
+	debug_usb_notext("\n");
+
 	return 0;
 exit:
 	return ret_val;
 }
 
-/* parse status and check if data are ready */
-enum data_status liqrf_check_data(unsigned char spi_status)
+/* write to output endpoint */
+int send_packet(struct usb_dev_handle *dev_handle, char *tx_buff, int tx_len)
 {
-	enum data_status stat = DATA_NOT_READY;
+	int ret_val = 0, i = 0;
 
-	switch (spi_status) {
-	case NO_MODULE_ON_USB:
-	case SPI_DISABLED:
-		fprintf(stderr, "SPI not working (disabled)\n");
-		break;
+	ret_val = usb_interrupt_write(dev_handle, OUT_EP_NR,
+				      tx_buff, tx_len,
+				      32000);
 
-	case SPI_CRCM_OK:
-		fprintf(stderr,
-			"Module not ready (full buffer, last CRCM ok)\n");
-		break;
-
-	case SPI_CRCM_ERR:
-		fprintf(stderr,
-			"Module not ready (full buffer, last CRCM error)\n");
-		break;
-
-	case COMMUNICATION_MODE:
-		fprintf(stderr, "Module ready - communication mode\n");
-		break;
-
-	case PROGRAMMING_MODE:
-		fprintf(stderr, "Module ready - programming mode\n");
-		break;
-
-	case DEBUG_MODE:
-		fprintf(stderr, "Module ready - debugging mode\n");
-		break;
-	default:
-		if ((spi_status & 0xC0) == 0x40)
-			stat = DATA_READY;
-	}
-
-	return stat;
-}
-
-/* count CRC for transmission */
-unsigned char count_CRC_tx(unsigned char *buff, int len)
-{
-	unsigned char crc_val;
-	int i = 0;
-
-	crc_val = 0x5F;
-
-	for (i = 1; i < len; i++)
-		crc_val ^= buff[i];
-
-	return crc_val;
-}
-
-/* count CRC for received buffer */
-unsigned char check_CRC_rx(unsigned char *buff, int PTYPE, int DLEN)
-{
-	unsigned char i, crc_val;
-
-	crc_val = 0x5F ^ PTYPE;
-
-	for (i = 2; i < DLEN + 2; i++)
-		crc_val ^= buff[i];
-
-	if (buff[DLEN + 2] == crc_val)
-		return 1;
-
-	return 0;
-}
-
-/* read and write to module via SPI */
-int liqrf_read_write(struct liqrf_obj *iqrf, int spi_check, int DLEN)
-{
-	int PTYPE = 0;
-	memset(iqrf->rx_buff, 0, sizeof(iqrf->rx_buff));
-
-	if (spi_check) {
-		iqrf->tx_buff[1] = 0x00;
-	} else {
-		if (iqrf->master_only_read)
-			PTYPE = DLEN & 0x7F;
-		else
-			PTYPE = DLEN | 0x80;
-
-		iqrf->tx_buff[1] = SPI_R_W;
-		iqrf->tx_buff[2] = PTYPE;
-		iqrf->tx_buff[DLEN + 3] = count_CRC_tx(iqrf->tx_buff, DLEN + 3);
-	}
-	iqrf->tx_buff[0] = CMD_FOR_CK;
-
-	iqrf->tx_len = DLEN + 5;
-	iqrf->rx_len = DLEN + 4;
-
-	if (liqrf_send_receive_packet(iqrf))
-		goto err;
-
-	memset(iqrf->tx_buff, 0, sizeof(iqrf->tx_buff));
-
-	if (!spi_check)
-		/* check CRC for received packet */
-		if (!check_CRC_rx(iqrf->rx_buff, PTYPE, DLEN))
-			return 0;
-err:
-	return 1;
-}
-
-/* signal handler 
-   usb read write is caled periodically
-*/
-void tmr_handler(signo)
-{
-
-	if (!liqrf_read_write(iqrf, SPI_CHECKING, 0))
+	if (ret_val < 0) {
+		fprintf(stderr, "Can't write to output endpoint\n");
 		goto exit;
-	
-	if ((liqrf_check_data(iqrf->rx_buff[iqrf->rx_len - 1]))
-	    == DATA_READY) {
-
-		iqrf->master_only_read = 1;
-		if (!liqrf_read_write(iqrf, SPI_NOT_CHECKING,
-				      iqrf->rx_buff[iqrf->rx_len - 1] & 0x3F))
-			goto exit;
 	}
+	debug_usb("Written data len = %d:\n", tx_len);
+	for (i = 0; i < tx_len; i++)
+		debug_usb_notext("%02X ", (unsigned char)tx_buff[i]);
+	debug_usb_notext("\n");
 exit:
-	return;
-
-}
-
-int main()
-{
-	timer_t t_id;
-	struct itimerspec time_spec = {
-	.it_interval = {.tv_sec = 1,.tv_nsec = 0},
-	.it_value = {.tv_sec = 1,.tv_nsec = 0}};
-
-	struct sigaction act;
-	sigset_t set;
-
-	sigemptyset(&set);
-	sigaddset(&set, SIGALRM);
-	/* register signal handler */
-	act.sa_flags = 0;
-	act.sa_mask = set;
-	act.sa_handler = &tmr_handler;
-
-	sigaction(SIGALRM, &act, NULL);
-
-	iqrf->dev = liqrf_device_init();
-
-	if (iqrf->dev == NULL) {
-		fprintf(stderr, "Could not init device\n");
-		goto exit;
-	}
-
-	usb_set_debug(dbg_level);
-
-	iqrf->dev_handle = usb_open(iqrf->dev);
-
-	if (iqrf->dev_handle == NULL) {
-		fprintf(stderr, "Could not open device\n");
-		goto exit;
-	}
-
-	if (timer_create(CLOCK_MONOTONIC, NULL, &t_id))
-		perror("timer_create");
-
-	if (timer_settime(t_id, 0, &time_spec, NULL))
-		perror("timer_settime");
-
-	while (err) ;
-
-exit:
-
-	return 0;
+	return ret_val;
 }
